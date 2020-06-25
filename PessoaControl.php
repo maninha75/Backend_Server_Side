6470540772453f9dc4ac8da45b345bb70f89868c
maninha75 <63216145+maninha75@users.noreply.github.com>
2020-06-24 23:27:08 -0300

    Add files via upload

---
+++ PessoaControl.php
@@ -0,0 +1,32 @@
+<?php
+include __DIR__.'/../model/Pessoa.php';
+
+class PessoaControl{
+	function insert($obj){		
+		$pessoa = new Pessoa();	
+		/* CHANCE DE MANIPULAR A REQUISIÇÃO ANTES DE ACESSAR O MODEL */			
+		return $pessoa->insert($obj);		
+	}
+
+	function update($obj,$id){
+		$pessoa = new Pessoa();
+		return $pessoa->update($obj,$id);
+	}
+
+	function delete($obj,$id){
+		$pessoa = new Pessoa();
+		return $pessoa->delete($obj,$id);
+	}
+
+	function find($id = null){
+		$pessoa = new Pessoa();
+		return $pessoa->find($id);
+	}
+
+	function findAll(){
+		$pessoa = new Pessoa();
+		return $pessoa->findAll();
+	}
+}
+
+?>
